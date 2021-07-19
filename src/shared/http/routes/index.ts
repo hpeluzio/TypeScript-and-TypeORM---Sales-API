5136a8edde4d2cfe81b7611b25b581fc7df12443
Henrique Peluzio <hpeluzio@gmail.com>
2021-07-19 02:02:40 -0300

    Update ProductController.ts, index.ts, and index.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
+import productsRouter from '@modules/products/routes';
 import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+routes.use('/products', productsRouter);
+
+// routes.get('/', (req, res) => {
+//   return res.json({ message: 'Hello World' });
+// });
 
 export default routes;
