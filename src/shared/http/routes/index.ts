a920b443894188c3899834a8b13927e318cdd004
Henrique Peluzio <hpeluzio@gmail.com>
2021-07-24 19:49:19 -0300

    Update customer.routes.ts and index.ts

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import sessionRouter from '@modules/users/routes/session.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import usersRouter from '@modules/users/routes/user.routes';
 import userProfileRouter from '@modules/users/routes/userprofile.routes';
+import customersRouter from '@modules/customers/routes/customer.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -12,6 +13,7 @@ routes.use('/users', usersRouter);
 routes.use('/session', sessionRouter);
 routes.use('/password', passwordRouter);
 routes.use('/userprofile', userProfileRouter);
+routes.use('/customers', customersRouter);
 
 // routes.get('/', (req, res) => {
 //   return res.json({ message: 'Hello World' });
